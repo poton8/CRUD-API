71d0ac91e95799fadc07fca9652ecd34bde612b4
poton8 <pagilestennis@gmail.com>
2019-07-01 18:59:07 -0500

    added post mapping and tested it with curl command

--- server.js
+++ server.js
@@ -1,92 +1,55 @@
 var fs = require('fs')
-var data = fs.readFileSync('FakeData.json')
-words = JSON.parse(data)
+var data = JSON.parse(fs.readFileSync('FakeData.json'))
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
-
+var uuidv4 = require('uuid/v4')
+employees = data.employees
 
 //Show server is running
 app.listen(3000, function listening(){
     console.log("listening")
+    console.log("hi")
 })
 
 //uses the html document inside the folder "Website"
 app.use(express.static('Website'))
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//Reads all employees
+app.get('/employees', (request, response)=>{
+    response.send(employees)
+})
 
 //Query for word in JSON file
-app.get('/find/:word', findWord)
-
-function findWord(request, response){
-    data = request.params
-    word = data.word
-    if (word in words){
-        response.send(word)
-    }
-    else{
-        response.send("word is not here")
-    }
-}
-
-//Create new word to add to JSON file
-app.get('/create/:word/:score?',function sendWord(request, response){
-    var data = request.params
-    var word = data.word
-    var score = Number(data.score)
-    if (!score){
-        response.send("number is required")
-    }
-
-    else{
-        words[word] = score
-        var data = JSON.stringify(words,null,2)
-        fs.writeFile('FakeData.json',data,finished)
-        
-
-        function finished(err){
-            console.log("No errors")
-            response.send("thank you for your word")
-        }
-    }
+app.get('/employees/:uuid', (request, response)=>{
+    uuid = request.params.uuid
+    var found = employees.find(element =>{
+        return element.uuid == uuid
+    })
+    response.send(found)
 })
 
-//Read all data in JSON File
-app.get('/readAll',function readAll(request, response){
-    response.send(words)
+//Create new employee
+app.post('/employees', (request, response) =>{
+    const id = uuidv4()
+    const employee = {
+        uuid: id,
+        name: request.body.name
+    }    
+    employees.push(employee)
+    response.send(employee)
 })
 
-//Update a words score in JSON file
-app.get('/update/:word/:score', function updateScore(request,response){
-    var data = request.params
-    var word = data.word
-    var score = Number(data.score)
+//update employee
+app.put('/employees/:uuid', (request, response) =>{
+    uuid = request.params.uuid
+    var found = employees.find(element =>{
+        return element.uuid == uuid
+    })
+    if(found){
 
-    if (word in words){
-        words[word] = score
-        var data = JSON.stringify(words, null, 2)
-        fs.writeFile('FakeData.json',data,function complete(err){
-            response.send("word updated")
-        })
-    }
-    else{
-        response.send("Word you are updating does not exist")
-    }
-})
-
-//delete a word in the JSON file
-app.get('/delete/:word', function deleteWord(request,response){
-    var data = request.params
-    var word = data.word
-
-    if (word in words){
-        delete words[word]
-        var data = JSON.stringify(words, null, 2)
-        fs.writeFile('FakeData.json',data, function success(err){
-            response.send("word has been deleted")
-        })
-    }
-    
-    else{
-        response.send("Word you are deleting does not exist")
     }
 })
